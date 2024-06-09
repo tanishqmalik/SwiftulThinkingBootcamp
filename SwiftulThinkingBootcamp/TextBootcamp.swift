d251c4f2af8cd5ba2d9fd721e5582f50224e8c4b
tanishqmalik <tanishqvideo@gmail.com>
2024-06-09 17:05:51 +0530

    okayy

---
+++ SwiftulThinkingBootcamp/TextBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  TextBootcamp.swift
+//  SwiftulThinkingBootcamp
+//
+//  Created by Tanishq Malik on 09/06/24.
+//
+
+import SwiftUI
+
+struct TextBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextBootcamp()
+}
