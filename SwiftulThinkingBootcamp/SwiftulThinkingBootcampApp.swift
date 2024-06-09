03888bb3d35106a356adf8c209951ca8fc68d94e
tanishq <tanishqvideo@gmail.com>
2024-06-09 15:03:28 +0530

    Initial Commit

---
+++ SwiftulThinkingBootcamp/SwiftulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulThinkingBootcampApp.swift
+//  SwiftulThinkingBootcamp
+//
+//  Created by Tanishq Malik on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
