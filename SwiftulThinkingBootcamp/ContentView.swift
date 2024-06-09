908703b0abaded55774b37dce82e6a1479288f77
tanishqmalik <tanishqvideo@gmail.com>
2024-06-09 16:04:49 +0530

    swiftie

--- SwiftulThinkingBootcamp/ContentView.swift
+++ SwiftulThinkingBootcamp/ContentView.swift
@@ -10,12 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+            Text("Hello, everyone!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color(hue: 1.0, saturation: 0.044, brightness: 0.316))
+                
+            Button("click me !!") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+                
         }
-        .padding()
+        
     }
 }
 
