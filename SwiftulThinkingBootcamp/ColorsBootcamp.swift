4f653e3c3eb618a38471b14e993b1182a36f007a
tanishqmalik <tanishqvideo@gmail.com>
2024-06-14 08:27:28 +0530

    theek hai

---
+++ SwiftulThinkingBootcamp/ColorsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ColorsBootcamp.swift
+//  SwiftulThinkingBootcamp
+//
+//  Created by Tanishq Malik on 14/06/24.
+//
+
+import SwiftUI
+
+struct ColorsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorsBootcamp()
+}
---
+++ SwiftulThinkingBootcamp/ShapesBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ShapesBootcamp.swift
+//  SwiftulThinkingBootcamp
+//
+//  Created by Tanishq Malik on 14/06/24.
+//
+
+import SwiftUI
+
+struct ShapesBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapesBootcamp()
+}
