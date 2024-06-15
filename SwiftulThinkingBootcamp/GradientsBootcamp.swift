5651577198229b91385cd632b264c507b0bc05fd
tanishqmalik <tanishqvideo@gmail.com>
2024-06-15 08:09:01 +0530

    gradients, icons, images...

---
+++ SwiftulThinkingBootcamp/GradientsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  GradientsBootcamp.swift
+//  SwiftulThinkingBootcamp
+//
+//  Created by Tanishq Malik on 15/06/24.
+//
+
+import SwiftUI
+
+struct GradientsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GradientsBootcamp()
+}
---
+++ SwiftulThinkingBootcamp/iconsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  iconsBootcamp.swift
+//  SwiftulThinkingBootcamp
+//
+//  Created by Tanishq Malik on 15/06/24.
+//
+
+import SwiftUI
+
+struct iconsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    iconsBootcamp()
+}
